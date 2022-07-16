66a80acb8c8281703d1e868cb16e5e91cc91bf2b
nittosantos <nitto_kun@hotmail.com>
2022-07-16 02:00:23 -0300

    feat: :sparkles: Adcionado json server e configurado

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { api } from "../../services/api";
+import { IProduct } from "../../store/modules/cart/types";
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector((state) => state);
-  console.log(catalog);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  return <h1>Catalog</h1>;
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => {
+        return (
+          <article key={product.id}>
+            <strong>{product.title}</strong> {" - "}
+            <span>{product.price}</span> {"  "}
+            <button type="button">Comprar</button>
+          </article>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Catalog;
