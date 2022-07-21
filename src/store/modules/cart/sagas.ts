0034d1de790fbaf941476b7b650c0e79d50718be
nittosantos <nitto_kun@hotmail.com>
2022-07-21 09:58:38 -0300

    feat: :fire: Pegando a quantidade de item adicionado

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,20 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCart } from "./actions";
 
-function checkProductStock() {
-  console.log("comprando");
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantidy: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantidy);
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
