4f5311af2fe8e9d8d93ef6fb919b78fda0294678
nittosantos <nitto_kun@hotmail.com>
2022-07-21 23:44:44 -0300

    style: :art: abstração com a actionTypes

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,8 +1,8 @@
-import { IProduct } from "./types";
+import { ActionTypes, IProduct } from "./types";
 
 export function addProductToCartRequest(product: IProduct) {
   return {
-    type: "ADD_PRODUCT_TO_CART_REQUEST",
+    type: ActionTypes.addProductToCartRequest,
     payload: {
       product,
     },
@@ -11,7 +11,7 @@ export function addProductToCartRequest(product: IProduct) {
 
 export function addProductToCartSucces(product: IProduct) {
   return {
-    type: "ADD_PRODUCT_TO_CART_SUCCES",
+    type: ActionTypes.addProductToCartSuccess,
     payload: {
       product,
     },
@@ -20,7 +20,7 @@ export function addProductToCartSucces(product: IProduct) {
 
 export function addProductToCartFailure(productId: Number) {
   return {
-    type: "ADD_PRODUCT_TO_CART_FAILURE",
+    type: ActionTypes.addProductToCartFailure,
     payload: {
       productId,
     },
